cb59951b54bad50d79e4235ce6526943fe0ea697
dhoarya7 <ridhoarya208@gmail.com>
2024-10-16 20:35:23 +0700

    First Commit

---
+++ main.js
@@ -0,0 +1,18 @@
+// Pastikan DOM telah dimuat sepenuhnya sebelum menjalankan script
+document.addEventListener('DOMContentLoaded', function () {
+    // Fungsi untuk memulai animasi pada elemen dengan ID tertentu
+    function startTypingAnimation(elementId, text, speed, onComplete) {
+      new Typed(elementId, {
+        strings: [text],
+        typeSpeed: speed,
+        showCursor: false,
+        onComplete: onComplete,
+      });
+    }
+  
+    // Memulai animasi untuk judul
+    startTypingAnimation('#typed-hello', 'Hello, world!', 50, function () {
+      // Setelah judul selesai, mulai animasi untuk deskripsi
+      startTypingAnimation('#typed-description', 'This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.', 30, null);
+    });
+  });
